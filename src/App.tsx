935d79fd992c34e1cc0812e7e91bf05a6dc7895a
rafaelspa <rsdepaula67@gmail.com>
2023-01-14 18:56:04 -0300

    Route structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,18 @@
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import Home from "./routes/Home";
+import FindUser from "./routes/Home/FindUser";
+import HomeBody from "./routes/Home/HomeBody";
+
 export default function App() {
-
   return (
-    <h1>App</h1>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home/>}>
+          <Route index element={<Navigate to="/home" />} />
+          <Route path="home" element={<HomeBody/>} />
+          <Route path="find-user" element={<FindUser/>} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
-
-
---
+++ src/routes/Home/FindUser/index.tsx
@@ -0,0 +1,7 @@
+export default function FindUser() {
+
+    return (
+
+        <h2>Find User</h2>
+    )
+}
